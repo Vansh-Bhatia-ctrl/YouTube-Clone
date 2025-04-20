d37ae1ac39dc6fee59dfafce01a5d3bf16c89705
Vansh-Bhatia-ctrl <vanshb066@gmail.com>
2025-04-21 01:22:03 +0530

    Initial commit: Add YouTube clone project files

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import Navbar from "./components/navbar";
+
+
+function App() {
+  return (
+    <>
+     <Navbar />
+
+      <div className="h-screen w-screen bg-customblack-100"></div> 
+    </>
+  );
+}
+
+export default App;
